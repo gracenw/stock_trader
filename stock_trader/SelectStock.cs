594a01fa406db9e19695c20e5d5d9918d7427ee1
uzumochi <48076750+uzumochi@users.noreply.github.com>
2021-04-15 14:39:59 -0400

    working on serial port

--- stock_trader/SelectStock.cs
+++ stock_trader/SelectStock.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RestSharp;
-using System.Net.Http;
+using System.Runtime.InteropServices;
 
 namespace stock_trader
 {
@@ -33,19 +33,20 @@ namespace stock_trader
 
         private void SendNewSymbol(object sender, EventArgs e)
         {
-            //upon clicking the Select button, a request sets the current symbol to the one selected
-            //HttpClient client = new HttpClient();
-            //client.BaseAddress = new Uri("https://cloud.iexapis.com/");
-            //HttpResponseMessage response = await client.GetAsync($"stable/stock/{SymbolBox.SelectedItem}/quote/latestPrice");
-            //response.EnsureSuccessStatusCode();
-            //string responseBody = await response.Content.ReadAsStringAsync();
+            using (System.IO.Ports.SerialPort port = new System.IO.Ports.SerialPort("/dev/ttyACM0", 9600))
+            {
+                port.Open();
+            }
 
+
+            //upon clicking the Select button, a request sets the current symbol to the one selected
             var client = new RestSharp.RestClient("https://cloud.iexapis.com");
             var request = new RestSharp.RestRequest($"/stable/stock/{SymbolBox.SelectedItem}/quote/latestPrice"); //change this for security measures
             request.AddParameter("token", "pk_423ad25e4bc94c58a425030b2d6edcfa"); //change this for security measures
-            var response = client.Get(request);
-            float latest_price = float.Parse(response.Content, System.Globalization.CultureInfo.InvariantCulture);
-            CurrentSymbol.Text = $"Current stock value: {latest_price}";
+            //var response = client.Get(request);
+            //float latest_price = float.Parse(response.Content, System.Globalization.CultureInfo.InvariantCulture);
+            //CurrentSymbol.Text = $"Current stock value: {latest_price}";
+            CurrentSymbol.Text = send_slope().ToString();
         }
     }
 }
